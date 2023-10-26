26cc443b125b2761e8ce623f0fc042962e6b8a60
José Córdova <jfernancordova@gmail.com>
2023-10-26 19:46:19 +0200

    Mailer

--- cmd/web/mailer.go
+++ cmd/web/mailer.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
 	"sync"
 	"time"
 
+	"github.com/vanng822/go-premailer/premailer"
 	mail "github.com/xhit/go-simple-mail/v2"
 )
 
-// Message represents an email message
 type Mail struct {
 	Domain      string
 	Host        string
@@ -17,14 +20,12 @@ type Mail struct {
 	Encryption  string
 	FromAddress string
 	FromName    string
-	// to be used for performing the actual sending of the mail
-	Wait      *sync.WaitGroup
-	MailChan  chan Message
-	ErrorChan chan error
-	DoneChan  chan bool
+	Wait        *sync.WaitGroup
+	MailerChan  chan Message
+	ErrorChan   chan error
+	DoneChan    chan bool
 }
 
-// Message represents an email message
 type Message struct {
 	From        string
 	FromName    string
@@ -36,8 +37,10 @@ type Message struct {
 	Template    string
 }
 
-func (m *Mail) sendMail(msg Message, errorChan chan error) error {
-	if msg.Template != "" {
+// a function to listen for messages on the MailerChan
+
+func (m *Mail) sendMail(msg Message, errorChan chan error) {
+	if msg.Template == "" {
 		msg.Template = "mail"
 	}
 
@@ -52,14 +55,17 @@ func (m *Mail) sendMail(msg Message, errorChan chan error) error {
 	data := map[string]any{
 		"message": msg.Data,
 	}
+
 	msg.DataMap = data
 
+	// build html mail
 	formattedMessage, err := m.buildHTMLMessage(msg)
 	if err != nil {
 		errorChan <- err
 	}
 
-	plainMessage, err := m.buildPlainMessage(msg)
+	// build plain text mail
+	plainMessage, err := m.buildPlainTextMessage(msg)
 	if err != nil {
 		errorChan <- err
 	}
@@ -81,12 +87,13 @@ func (m *Mail) sendMail(msg Message, errorChan chan error) error {
 
 	email := mail.NewMSG()
 	email.SetFrom(msg.From).AddTo(msg.To).SetSubject(msg.Subject)
+
 	email.SetBody(mail.TextPlain, plainMessage)
 	email.AddAlternative(mail.TextHTML, formattedMessage)
 
 	if len(msg.Attachments) > 0 {
-		for _, attachment := range msg.Attachments {
-			email.AddAttachment(attachment)
+		for _, x := range msg.Attachments {
+			email.AddAttachment(x)
 		}
 	}
 
@@ -94,24 +101,74 @@ func (m *Mail) sendMail(msg Message, errorChan chan error) error {
 	if err != nil {
 		errorChan <- err
 	}
-
-	return nil
 }
 
 func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
-	return "", nil
+	templateToRender := fmt.Sprintf("./cmd/web/templates/%s.html.gohtml", msg.Template)
+
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	formattedMessage := tpl.String()
+	formattedMessage, err = m.inlineCSS(formattedMessage)
+	if err != nil {
+		return "", err
+	}
+
+	return formattedMessage, nil
 }
 
-func (m *Mail) buildPlainMessage(msg Message) (string, error) {
-	return "", nil
+func (m *Mail) buildPlainTextMessage(msg Message) (string, error) {
+	templateToRender := fmt.Sprintf("./cmd/web/templates/%s.plain.gohtml", msg.Template)
+
+	t, err := template.New("email-plain").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+
+	plainMessage := tpl.String()
+
+	return plainMessage, nil
+}
+
+func (m *Mail) inlineCSS(s string) (string, error) {
+	options := premailer.Options{
+		RemoveClasses:     false,
+		CssToAttributes:   false,
+		KeepBangImportant: true,
+	}
+
+	prem, err := premailer.NewPremailerFromString(s, &options)
+	if err != nil {
+		return "", err
+	}
+
+	html, err := prem.Transform()
+	if err != nil {
+		return "", err
+	}
+
+	return html, nil
 }
 
 func (m *Mail) getEncryption(e string) mail.Encryption {
 	switch e {
-	case "ssl":
-		return mail.EncryptionSSLTLS
 	case "tls":
 		return mail.EncryptionSTARTTLS
+	case "ssl":
+		return mail.EncryptionSSLTLS
 	case "none":
 		return mail.EncryptionNone
 	default:
--- go.mod
+++ go.mod
@@ -9,10 +9,16 @@ require (
 	github.com/gomodule/redigo v1.8.9
 	github.com/jackc/pgconn v1.14.1
 	github.com/jackc/pgx/v4 v4.18.1
+	github.com/vanng822/go-premailer v1.20.0
+	github.com/xhit/go-simple-mail/v2 v2.16.0
 	golang.org/x/crypto v0.14.0
 )
 
 require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.1.0 // indirect
+	github.com/go-test/deep v1.1.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -20,7 +26,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
-	github.com/vanng822/go-premailer v1.20.2 // indirect
-	github.com/xhit/go-simple-mail/v2 v2.16.0 // indirect
+	github.com/vanng822/css v1.0.1 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
